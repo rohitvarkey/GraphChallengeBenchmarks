4041add3b8388f0fdedccdb96198fc33d858617c
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-03-08 03:35:35 -0500

    Ready for PACE

--- bench.jl
+++ bench.jl
@@ -8,7 +8,7 @@ end
 function bench(T, num_nodes, output_file)
     precompile_run(T)
     results = static_partition_experiment(T, num_nodes)
-    jldopen(output_file, "w") do file
-        write(file, "result", results)
+    open(output_file, "w") do file
+        write(file, "$results")
     end
 end
--- generate_scripts.jl
+++ generate_scripts.jl
@@ -21,23 +21,13 @@ function qsub_header(nthread, job, type_name, num_nodes, useremail="", queue="")
     #PBS -j oe
     module load gcc/5.3.0
     module load julia/6.2.0
-    export JULIA_NUM_THREADS=$(nthreads)
+    export JULIA_NUM_THREADS=$(nthread)
     """
     header
 end
 
-function exportenvs()
-    curdir = dirname(@__FILE__)
-    stingerlibpath = joinpath(dirname(curdir), "lib", "stinger", "lib")
-    envstring = """
-    export STINGER_LIB_PATH=$(stingerlibpath)
-    """
-    envstring
-end
-
 #Generate all the kronecker graphs
 function runbench(nthreads, num_nodes, types; qsub=true, useremail="", queue="")
-
     #Create the directories
     create_directories(["input", "output", "scripts"])
     types_output_dir = Dict(
@@ -56,24 +46,26 @@ function runbench(nthreads, num_nodes, types; qsub=true, useremail="", queue="")
     curdir = dirname(@__FILE__)
     outputdir = joinpath(curdir, "output")
     scriptdir = joinpath(curdir, "scripts")
-    curdir = dirname(@__FILE__)
     benchfile = joinpath(curdir, "bench.jl")
     masterscripthandle = open(joinpath(scriptdir, "master_script"), "w")
     for T in types
-        for n in num_nodes
-            output_file = joinpath(outputdir, types_output_dir[T], "$(types_output_dir[T])_$n.jld)")
-            script = """#!/bin/bash
-            $(if qsub qsub_header(nthread, "gc", T, n, useremail, queue) else "" end)
-            $(exportenvs())
-            julia -O3 --check-bounds=no -e 'include("$benchfile"); run_bench($T, $(n), $(output_file))'
-            """
-            open(joinpath(scriptdir, types_output_dir[T], "$(types_output_dir[T])_$n"), "w") do f
-                write(f, script)
-            end
-            if qsub
-                run(`qsub $(joinpath(scriptdir, types_output_dir[T], "$(types_output_dir[T])_$n"))`)
-            else
-                write(masterscripthandle, """bash $(joinpath(scriptdir, types_output_dir[T], "$(types_output_dir[T])_$n"))\n""")
+        for nthread in nthreads
+            for n in num_nodes
+                output_file = joinpath(outputdir, types_output_dir[T], "$(types_output_dir[T])_$n")
+                script = """#!/bin/bash
+                $(if qsub qsub_header(nthread, "gc", T, n, useremail, queue) else "" end)
+                julia -O3 --check-bounds=no -e 'include("$benchfile"); bench($T, $(n), "$(output_file)")'
+                """
+                open(joinpath(scriptdir, types_output_dir[T], "$(types_output_dir[T])_$n"), "w") do f
+                    write(f, script)
+                end
+                #=
+                if qsub
+                    run(`qsub $(joinpath(scriptdir, types_output_dir[T], "$(types_output_dir[T])_$n"))`)
+                else
+                    write(masterscripthandle, """bash $(joinpath(scriptdir, types_output_dir[T], "$(types_output_dir[T])_$n"))\n""")
+                end
+                =#
             end
         end
     end
