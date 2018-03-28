2333556a4f53ac7135e7f1f39bbf4311e86fdee1
Rohit Varkey Thankachan <rohitvarkey@gmail.com>
2018-03-28 05:20:41 -0400

    Plots

--- analyze.jl
+++ analyze.jl
@@ -1,4 +1,4 @@
-using CSV, DataFrames
+using CSV, DataFrames, Query
 
 include("generate_scripts.jl")
 
@@ -12,4 +12,45 @@ end
 function collect_stats(types = keys(TYPES_OUTPUT_DIR))
     dfs = [type_stats(T) for T in types]
     vcat(dfs...)
+end
+
+function create_plots(df)
+    plots = []
+    threads = sort(unique(df[:threads]))
+    for thread in threads
+        filtered_df = df |>
+            @filter(_.threads == thread) |>
+            @orderby_descending(-_.num_nodes)
+        p = @df filtered_df plot(:num_nodes, :time, group=:Name, scale=:log10, marker=:auto, title="Threads = $thread", legend = :topleft)
+        push!(plots, p)
+    end
+    final_plot = plot(plots..., size = (1200, 800))
+    savefig(final_plot, "thread_times.png")
+end
+
+
+function create_scaling_plots(df)
+    plots = []
+    for (T, t_name) in GraphChallenge.BACKEND_NAMES
+        filtered_df = df |>
+            @filter(_.Name == t_name) |>
+            @orderby_descending(-_.threads)
+        single_thread_runs = @from i in filtered_df begin
+               @where i.threads == 1
+               @orderby i.num_nodes
+               @select i.num_nodes => i.time
+               @collect
+        end
+        scaling_numbers = @from i in filtered_df begin
+               @select i.num_nodes => i.time
+               @collect
+        end
+        if isempty(filtered_df)
+            continue
+        end
+        p = @df filtered_df plot(:threads, :time, group=:num_nodes, scale=:log10, marker=:auto, title="Type = $(t_name)", legend = :topleft)
+        push!(plots, p)
+    end
+    final_plot = plot(plots..., size = (1200, 800))
+    savefig(final_plot, "thread_scaling.png")
 end
